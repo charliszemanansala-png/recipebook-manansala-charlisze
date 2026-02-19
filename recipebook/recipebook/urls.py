2f26ad391576687cfbabbfa823a3f3b2b4de171d
Charlisze Manansala <charlisze.manansala@student.ateneo.edu>
2026-02-19 23:09:40 +0800

    Lab2: made changes in recipebook/urls

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('recipes/', include('ledger.urls', namespace = "ledger")),
-    path('recipe/', include('ledger.urls', namespace = "ledger")),
+    path('', include('ledger.urls', 'ledger')),
     path('admin/', admin.site.urls),
 ]
