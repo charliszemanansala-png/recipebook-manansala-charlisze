8aad76c802c627dce4d64636a3ef26894513c024
Charlisze Manansala <charlisze.manansala@student.ateneo.edu>
2026-02-19 23:03:59 +0800

    Lab2: Made changes in models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name= models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.id)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.id)])
+    
+class Recipe_Ingredient(models.Model):
+    quantity = models.CharField(max_length = 100)
+
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete = models.CASCADE,
+        related_name = 'recipe',
+    )
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete = models.CASCADE,
+        related_name = 'ingredients',
+    )
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient} for {self.recipe}"
