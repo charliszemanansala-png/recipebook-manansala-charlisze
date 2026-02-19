16373af0651f87fe06fddfd1eccefad3048ab582
Charlisze Manansala <charlisze.manansala@student.ateneo.edu>
2026-02-19 23:07:17 +0800

    Lab2: made changes in admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Recipe, Recipe_Ingredient
+
+class Recipe_IngredientInLine(admin.TabularInline):
+    model = Recipe_Ingredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display =('id', 'name')
+    search_fields = ("name", )
+    inlines = [Recipe_IngredientInLine,]
+
+class Recipe_IngredientAdmin(admin.ModelAdmin):
+    model = Recipe_Ingredient
+    search_fields = ('recipe__name', 'ingredient__name' )
+    list_display = ('recipe', 'ingredient', 'quantity' )
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Recipe_Ingredient, Recipe_IngredientAdmin)
+
 # Register your models here.
